262892f998b764f17797d6ba9703839da5956341
Tony Chen <chenzy_0513@hotmail.com>
2018-07-21 22:31:08 +1000

    minor fix

--- AlgoTest/BinarySearch_MinPeoplePerClinic.cpp
+++ AlgoTest/BinarySearch_MinPeoplePerClinic.cpp
@@ -19,6 +19,14 @@ int predicate1(vector<int>& vecPeopleOfCities, int x)
 
 int FindMinMaxPeopleForSingleClinic(vector<int>& vecPeopleOfCities, int numClinics)
 {
+    // If the input people per city vector is empty, or the number of clinic is non-positive, treat it as invalid input and return 0.
+    if (vecPeopleOfCities.size() == 0 || numClinics <= 0)
+        return 0;
+    // If any number of people in cities is non-positive, treat is as invalid input and return 0.
+    for (const auto& i : vecPeopleOfCities)
+        if (i <= 0)
+            return 0;
+    
     // Convert the problem to the predicate - from a given maximum number(x) of people per clinic, required clinics are smaller or equal to a certain number.".
     // For this predicate, if p(x) is true, then if x increases to y, p(y) must also be true because the more maximum people per clinic, 
     // the process capability increases so that the same number of people in all cities must be met by the same number of clinics. Therefore, Binary search can be applied.
@@ -46,4 +54,16 @@ TEST(BinarSearch2, MinMaxPeoplePerClinic)
 {
     vector<int> vecPeopleOfCities = { 200000, 500000 };
     EXPECT_EQ(FindMinMaxPeopleForSingleClinic(vecPeopleOfCities, 7), 100000);
+    
+    vecPeopleOfCities = { 10000, 20000, 30000, 40000, 30000, 10000};
+    EXPECT_EQ(FindMinMaxPeopleForSingleClinic(vecPeopleOfCities, 10), 20000);
+    
+    vecPeopleOfCities = { 10000 };
+    EXPECT_EQ(FindMinMaxPeopleForSingleClinic(vecPeopleOfCities, 5), 2000);
+    
+    vecPeopleOfCities = {};
+    EXPECT_EQ(FindMinMaxPeopleForSingleClinic(vecPeopleOfCities, 1), 0);
+    
+    vecPeopleOfCities = { -200 };
+    EXPECT_EQ(FindMinMaxPeopleForSingleClinic(vecPeopleOfCities, 1), 0);
 }
